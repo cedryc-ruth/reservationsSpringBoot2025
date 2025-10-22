95b3cb77d5bee02167bb45999b18967f3477fd9e
Cédryc Ruth <ceruth@epfc.eu>
2025-10-22 14:42:35 +0200

    After framework installation

---
+++ src/test/java/be/iccbxl/pid/reservations_springboot/ReservationsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package be.iccbxl.pid.reservations_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
