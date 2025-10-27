a058fffb9bed6c9c0011c50759bb7662ac3aaaec
Cédryc Ruth <ceruth@epfc.eu>
2025-10-27 11:16:29 +0100

    Controller Artist +templates +routes

---
+++ src/main/java/be/iccbxl/pid/reservations_springboot/controller/ArtistController.java
@@ -0,0 +1,38 @@
+package be.iccbxl.pid.reservations_springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import be.iccbxl.pid.reservations_springboot.model.Artist;
+import be.iccbxl.pid.reservations_springboot.service.ArtistService;
+
+@Controller
+public class ArtistController {
+	@Autowired
+	ArtistService service;
+
+	@GetMapping("/artists")
+	public String index(Model model) {
+		List<Artist> artists = service.getAllArtists();
+        
+        model.addAttribute("artists", artists);
+	    model.addAttribute("title", "Liste des artistes");
+		
+	    return "artist/index";
+    }
+
+    @GetMapping("/artists/{id}")
+    public String show(Model model, @PathVariable("id") long id) {
+	    Artist artist = service.getArtist(id);
+
+	    model.addAttribute("artist", artist);
+	    model.addAttribute("title", "Fiche d'un artiste");
+		
+        return "artist/show";
+    }
+}
