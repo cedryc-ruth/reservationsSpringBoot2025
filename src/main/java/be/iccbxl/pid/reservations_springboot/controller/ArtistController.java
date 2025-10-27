a3590226c34964b9c38b6d9e93d15893b917d293
Cédryc Ruth <ceruth@epfc.eu>
2025-10-27 11:44:23 +0100

    Form create Artist

--- src/main/java/be/iccbxl/pid/reservations_springboot/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/controller/ArtistController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -75,5 +77,25 @@ public class ArtistController {
         service.updateArtist(id, artist);	    
     
 		return "redirect:/artists/"+artist.getId();
+	}
+
+    @GetMapping("/artists/create")
+	public String create(Model model) {
+	    Artist artist = new Artist();
+
+	    model.addAttribute("artist", artist);
+		
+	    return "artist/create";
+	}
+	
+	@PostMapping("/artists/create")
+	public String store(@Valid @ModelAttribute Artist artist, BindingResult bindingResult, Model model) {
+	    if (bindingResult.hasErrors()) {
+		    return "artist/create";
+	    }
+		    
+	    service.addArtist(artist);
+	    
+	    return "redirect:/artists/"+artist.getId();
 	}
 }
